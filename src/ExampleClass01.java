f106196c99842d5bc3855a3a3571b87c7ed734ed
mmazepa <maksymilianmariusz@gmail.com>
2018-11-06 02:21:48 +0100

    Dodanie opjaśnień punktowych do WTFCode.

--- src/ExampleClass01.java
+++ src/ExampleClass01.java
@@ -1,18 +1,18 @@
 class ExampleClass01
 {
-  public static int wtf_maker(int a, int b, int c)
+  public static int wtf_maker(int a, int b, int c)                // 1 pkt (metoda)
   {
-    if (a > 0 && b > 0 && c > 0) wtf_maker(a-1, b-1, c-1);
-    else if (a > 0 && b > 0 && c < 0) wtf_maker(a-1, b-1, c+1);
-    else if (a > 0 && b < 0 && c > 0) wtf_maker(a-1, b+1, c-1);
-    else if (a > 0 && b < 0 && c < 0) wtf_maker(a-1, b+1, c+1);
-    else if (a < 0 && b > 0 && c > 0) wtf_maker(a+1, b-1, c-1);
-    else if (a < 0 && b > 0 && c < 0) wtf_maker(a+1, b-1, c+1);
-    else if (a < 0 && b < 0 && c > 0) wtf_maker(a+1, b+1, c-1);
-    else if (a < 0 && b < 0 && c < 0) wtf_maker(a+1, b+1, c+1);
-    else if (a == 0 && b == 0 && c == 0) return 0;
+    if (a > 0 && b > 0 && c > 0) wtf_maker(a-1, b-1, c-1);        // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a > 0 && b > 0 && c < 0) wtf_maker(a-1, b-1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a > 0 && b < 0 && c > 0) wtf_maker(a-1, b+1, c-1);   // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a > 0 && b < 0 && c < 0) wtf_maker(a-1, b+1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a < 0 && b > 0 && c > 0) wtf_maker(a+1, b-1, c-1);   // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a < 0 && b > 0 && c < 0) wtf_maker(a+1, b-1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a < 0 && b < 0 && c > 0) wtf_maker(a+1, b+1, c-1);   // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a < 0 && b < 0 && c < 0) wtf_maker(a+1, b+1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
+    else if (a == 0 && b == 0 && c == 0) return 0;                // 3 pkt (po 1 pkt za każde wyrażenie)
     return 0;
-  }
+  }                                                               // razem: 28 pkt
 
   public static void main (String[] args)
   {
