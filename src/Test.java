9dcd95a77af2310ea9b0f0cc856acce8803f2b3f
mmazepa <maksymilianmariusz@gmail.com>
2018-11-13 11:19:50 +0100

    Wrzutka z zajęć (13.11.2018).

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test
+{
+  public static void main (String[] args)
+  {
+    // System.out.println(true.getClass()); // error: boolean cannot be dereferenced
+    System.out.println(new Boolean(true).getClass()); // class java.lang.Boolean
+    System.out.println(new Boolean(true) instanceof Boolean); // true
+
+    System.out.println(0==0); // true
+    // System.out.println((0==0).getClass()); // error: boolean cannot be dereferenced
+  }
+}
