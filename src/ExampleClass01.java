c1ff7bf1e6df6cc155a8338392e3883ad87dfc36
mmazepa <maksymilianmariusz@gmail.com>
2018-11-13 02:02:05 +0100

    Aktualizacja WTFCode.

--- src/ExampleClass01.java
+++ src/ExampleClass01.java
@@ -1,21 +1,16 @@
-class ExampleClass01
-{
-  public static int wtf_maker(int a, int b, int c)                // 1 pkt (metoda)
-  {
-    if (a > 0 && b > 0 && c > 0) wtf_maker(a-1, b-1, c-1);        // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a > 0 && b > 0 && c < 0) wtf_maker(a-1, b-1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a > 0 && b < 0 && c > 0) wtf_maker(a-1, b+1, c-1);   // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a > 0 && b < 0 && c < 0) wtf_maker(a-1, b+1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a < 0 && b > 0 && c > 0) wtf_maker(a+1, b-1, c-1);   // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a < 0 && b > 0 && c < 0) wtf_maker(a+1, b-1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a < 0 && b < 0 && c > 0) wtf_maker(a+1, b+1, c-1);   // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a < 0 && b < 0 && c < 0) wtf_maker(a+1, b+1, c+1);   // 3 pkt (po 1 pkt za każde wyrażenie)
-    else if (a == 0 && b == 0 && c == 0) return 0;                // 3 pkt (po 1 pkt za każde wyrażenie)
-    return 0;
-  }                                                               // razem: 28 pkt
-
-  public static void main (String[] args)
-  {
-    wtf_maker(10, 20, 30);
-  }
-}
+// metoda zliczająca ilość potrzebnych kroków, aby wrócić do punktu (0,0,0)
+// przykładowe wykorzystanie: gra wykorzystująca układ współrzędnych
+int steps = 0;
+public static int count_steps(int x, int y, int z) {                           // 1 pkt (metoda)
+  if (x > 0 && y > 0 && z > 0) { steps += 3; count_steps(x-1, y-1, z-1); }     // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+  else if (x > 0 && y > 0 && z == 0) { steps += 2; count_steps(x-1, y-1, z); } // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+  else if (x > 0 && y == 0 && z > 0) { steps += 2; count_steps(x-1, y, z-1); } // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+  else if (x > 0 && y == 0 && z == 0) { steps += 1; count_steps(x-1, y, z); }  // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+  else if (x == 0 && y > 0 && z > 0) { steps += 2; count_steps(x, y-1, z-1); } // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+  else if (x == 0 && y > 0 && z == 0) { steps += 1; count_steps(x, y-1, z); }  // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+  else if (x == 0 && y == 0 && z > 0) { steps +=1;  count_steps(x, y, z-1);}   // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+  else if (x == 0 && y == 0 && z == 0) return steps;                           // 3 pkt : 1 pkt za if + po 1 pkt za "&&"
+}                                                                              // razem: 25 pkt
+// przykładowe wywołanie:
+// metoda zwraca ilość kroków z (10,20,15) do (0,0,0) najkrótszą drogą
+int steps = count_steps(10,20,15);
